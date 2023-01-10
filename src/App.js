23629814d1ff8c73db4388a53b436cdc03902c21
Anthony Serquén <srquen77@gmail.com>
2023-01-10 10:28:01 -0500

    Se agregaron las fuentes y se instalaron los paquetes de framemotion (quise hacer animaciones xd) gh-pages y webfontloader

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 // import logo from './logo.svg';
+import WebFont from 'webfontloader';
 import './App.css';
 import Contenido from './components/Contenido';
 import Footer from './components/Footer';
 import Header from "./components/Header";
+
+WebFont.load({
+  google: {
+    families: ['Work Sans:400, 500, 700', 'sans-serif']
+  }
+});
 
 function App() {
   return (
